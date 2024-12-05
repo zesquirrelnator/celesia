551d6b6d3adca08c8513d5ec79d1ff3dc7086714
zesquirrelnator <44183760+zesquirrelnator@users.noreply.github.com>
2024-12-05 14:05:17 -0500

    Fixed Get Started Button

--- app/landing-components/GetStarted.tsx
+++ app/landing-components/GetStarted.tsx
@@ -1,18 +1,21 @@
 "use client";
 import { posthogClient } from '@/lib/posthog'
-import Link from 'next/link'
 import React from 'react'
 import { FaArrowRight } from 'react-icons/fa6'
 
 export default function GetStarted() {
-  return (
-    <Link
-        href="https://app.celesia.io/"
-        onClick={() => posthogClient.capture('cta_clicked', { location: 'Hero' })}
-        className='flex items-center duration-300 ease-in-out font-semibold text-white bg-blue-400 hover:bg-blue-500 dark:text-white px-4 py-2 rounded-lg transform hover:scale-105'
-    >
-        Get Started <FaArrowRight size='1.1rem' className='ml-1' />
-    </Link>
-  )
-}
-
+    const handleClick = () => {
+      posthogClient.capture("cta_clicked", { location: "Hero" });
+      console.log("clicked!");
+      window.location.href = "https://app.celesia.io/";
+    };
+  
+    return (
+      <button
+        onClick={handleClick}
+        className="flex items-center duration-300 ease-in-out font-semibold text-white bg-blue-400 hover:bg-blue-500 dark:text-white px-4 py-2 rounded-lg transform hover:scale-105"
+      >
+        Get Started <FaArrowRight size="1.1rem" className="ml-1" />
+      </button>
+    );
+  }
