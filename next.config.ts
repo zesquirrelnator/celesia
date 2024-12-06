844d8bcc1034b465c1ed42cb7d0c830e62b8bf90
zesquirrelnator <44183760+zesquirrelnator@users.noreply.github.com>
2024-12-05 21:09:22 -0500

    fixed link protocol*

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
